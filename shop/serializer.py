f2d5898a4594394a15c5febde656341171e60c84
pcgrimal <pcgrimal@gmail.com>
2022-07-11 17:43:31 +0200

    Add Disable feature

--- shop/serializer.py
+++ shop/serializer.py
@@ -1,16 +1,47 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from shop.models import Article, Category, Product
 
-class CategorySerializer(ModelSerializer):
+
+class CategoryListSerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'date_created', 'date_updated']
+
+class CategoryDetailSerializer(ModelSerializer):
+    # En utilisant un 'SerializerMethodField', il est nécessaire d'écrire une méthode nommée 'get_XXX' où XXX est le nom de l'attribut, ici 'products'
+    products = SerializerMethodField()
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'date_created', 'date_updated', 'products']
         #fields = '__all__'
 
-class ProductSerializer(ModelSerializer):
+    def get_products(self, instance):
+        # Le paramètre 'instance' est l'instance de la catégorie consultée.
+        # Dans le cas d'une liste, cette méthode est appelée autant de fois qu'il y a
+        # d'entités dans la liste
+
+        # On applique le filtre sur notre queryset pour n'avoir que le produits actifs
+        queryset = instance.products.filter(active=True)
+        # Le serializer est créé avec le queryset défini et toujours défini en tant que many=True
+        serializer = ProductListSerializer(queryset, many=True)
+        # La propriété '.data' est le rendu de notre serializer que nous retournons ici
+        return serializer.data
+
+class ProductListSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'date_created', 'date_updated']
+
+class ProductDetailSerializer(ModelSerializer):
+    articles = SerializerMethodField()
     class Meta:
          model = Product
-         fields = ['id', 'name', 'date_created', 'date_updated', 'category']
+         fields = ['id', 'name', 'date_created', 'date_updated', 'category', 'articles']
+
+    def get_articles(self, instance):
+        queryset = instance.articles.filter(active=True)
+        serializer = ArticleSerializer(queryset, many=True)
+        return serializer.data
 
 class ArticleSerializer(ModelSerializer):
     class Meta:
--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,40 @@
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from shop.models import Article, Category, Product
-from shop.serializer import ArticleSerializer, CategorySerializer, ProductSerializer
+from shop.serializer import ArticleSerializer, CategoryDetailSerializer, CategoryListSerializer, ProductDetailSerializer, ProductListSerializer
 
 
-class CategoryViewset(ReadOnlyModelViewSet):
+class MultipleSerializerMixin:
+    # Une mixin est une classe qui ne fonctionne pas de façon autonome
+    # Elle permet d'ajouter des fonctionnalités aux classes qui les étendent
+    detail_serializer_class = None
 
-    serializer_class = CategorySerializer
+    def get_serializer_class(self):
+        # Notre mixin détermine quel serializer à utiliser même si elle ne sait pas ce que c'est ni comment l'utiliser
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            # Si l'action demandée est le détail alors nous retournons le serializer de détail
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
+class CategoryViewset(MultipleSerializerMixin,ReadOnlyModelViewSet):
+
+    serializer_class = CategoryListSerializer
+    # Ajoutons un attribut de classe qui nous permet de définir notre serializer de détail
+    detail_serializer_class = CategoryDetailSerializer
 
     def get_queryset(self):
         return Category.objects.filter(active=True)
 
-class ProductViewset(ReadOnlyModelViewSet):
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
 
-    serializer_class = ProductSerializer
+class ProductViewset(MultipleSerializerMixin,ReadOnlyModelViewSet):
+
+    serializer_class = ProductListSerializer
+    detail_serializer_class = ProductDetailSerializer
 
     def get_queryset(self):
         queryset = Product.objects.filter(active=True)
@@ -21,6 +43,11 @@ class ProductViewset(ReadOnlyModelViewSet):
         if category_id is not None:
             queryset = queryset.filter(category_id=category_id)
         return queryset
+
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
 
 class ArticleViewset(ReadOnlyModelViewSet):
 
